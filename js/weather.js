081b0b25366ae8f85d2b5997f2cbfe9b7a7b2adf
Charles VIGNAL <charles.vignal@gmail.com>
2021-04-23 12:01:31 +0200

    [mod] Changed links

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '5d65b74cabd2f4f36204d4e159f30cf9';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 48.856613;
+  let longitude = 2.352222;
 
   getWeather(latitude, longitude);
 }
@@ -30,8 +30,6 @@ function setPosition(position) {
 // Get the Weather data
 function getWeather(latitude, longitude) {
   let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-
-  console.log(api);
 
   fetch(api)
     .then(function (response) {
